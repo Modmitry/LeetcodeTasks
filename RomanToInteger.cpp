8a88de379f7bdf48078cf77b113dbc2e2121435f
Modmitry <86513190+Modmitry@users.noreply.github.com>
2021-11-27 21:07:24 +0300

    Create RomanToInteger.cpp

---
+++ RomanToInteger.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int romanToInt(string s) 
+    {
+        int num=0;
+        
+        for (int i=0;i<s.length();i++)
+        {
+            if (s[i]=='I')
+            {
+                if (i!=s.length()-1)
+                {
+                if ((s[i+1]=='V')||(s[i+1]=='X'))
+                    num-=1;
+                else num+=1; 
+                }
+                else  num+=1; 
+            }
+            
+             if (s[i]=='X')
+            {
+                if (i!=s.length()-1)
+                {
+                if ((s[i+1]=='L')||(s[i+1]=='C'))
+                    num-=10;
+                else num+=10; 
+                }
+                else  num+=10; 
+            }
+            
+            
+            if (s[i]=='C')
+            {
+                if (i!=s.length()-1)
+                {
+                if ((s[i+1]=='D')||(s[i+1]=='M'))
+                    num-=100;
+                else num+=100; 
+                }
+                else  num+=100; 
+            }
+            
+            
+             if (s[i]=='V') num+=5;
+             if (s[i]=='L') num+=50;
+             if (s[i]=='D') num+=500;
+             if (s[i]=='M') num+=1000;
+                    
+        }
+               
+       return num; 
+    }
+};
